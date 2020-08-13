397dd6b93711ab67c8b92464c0250122e1bbb6f8
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-13 02:49:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -40,4 +40,11 @@ def highest_tip
         end
         highest_tipper
     end
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
   end
+ 
+  best_tipped_meal.customer
+end
+end
